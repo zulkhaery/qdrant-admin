275d2467c4957f689072ed112793c69ab3a91454
Zul <zulkhaery@gmail.com>
2026-05-08 15:01:50 +0700

    remove favicon

--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <title>Qdrant Admin - <?= $title ?></title>
-    <link rel="icon" type="image/png" href="/favicon.png">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
      <link rel="stylesheet" href="/css/style.css">
 </head>
