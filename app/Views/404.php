8657d8e58add4d14c9f83decade63c085e0c305b
Zul <zulkhaery@gmail.com>
2026-05-09 18:24:53 +0700

    add 404

--- app/Views/404.php
+++ app/Views/404.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<title>404</title>
+<title>404 Not Found</title>
 
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -132,10 +132,9 @@ body {
 <div class="container">
     <div class="error-code">404</div>
 
-    <h1 class="title">Page Not Found</h1>
 
     <p class="description">
-       While you're here, <b>enjoy this completely useless placeholder text</b>. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vel accumsan sapien, sed dapibus tortor. Maecenas velit nulla, dapibus quis consectetur ut, molestie sit amet mauris. Thankyou.
+       While you're here, <b>enjoy this completely useless placeholder text</b>. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vel accumsan sapien, sed dapibus tortor. Maecenas velit nulla, dapibus quis consectetur ut, molestie sit amet mauris.
     </p>
 
 
